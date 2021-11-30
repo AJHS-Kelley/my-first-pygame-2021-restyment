1c31bd6e13b4f3ce69ae0f4c838080cd9ac22464
Geo P <90413348+restyment@users.noreply.github.com>
2021-11-30 14:35:35 -0500

    v0.3
    added colors and color values

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -9,3 +9,12 @@ pygame.init()
 # Setup the game window.
 windowSurface = pygame.display.set_mode((500,400), 0, 32)
 pygame.display.set_captain('Hello, world!')
+
+# Setup color values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+CYANBLUE = (80, 255, 250)
+VAMPPURP = (105, 2, 137)
